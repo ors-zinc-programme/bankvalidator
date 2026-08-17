77afe5422a1a54ea0b3ce2892f66dc7f4ae701c7
elnasirahmadmuhammad1992 <elnasirahmadmuhammad1992@gmail.com>
2026-08-17 21:25:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bank Account Validator - offline shell cache
-const CACHE = "cbd-validator-v12";
+const CACHE = "cbd-validator-v14";
 const SHELL = [
   "validator.html",
   "manifest.webmanifest",
